1116df1a7c69dc3f196338cf431e87cbc77f4155
Arcyvilk <blazemakerrr@gmail.com>
2017-05-20 13:26:02 +0200

    New game comment added

--- classes/command.js
+++ classes/command.js
@@ -73,8 +73,9 @@ exports.Command = function (data) {
     command.listOfEmoteReactionResponses = {
         'Ⓜ': ':mm:310140119606886421'
     };
-    command.arrayOfFetus = ["None of you could beat that lol if u change your mind appreciate",
-                            "Almost as good as Clicker Heroes",
-                            "Why should you torture yourself with this game when you can do 100 easy ones?",
-                            "Many friends also say this is a hardcore game :D"];
+    command.arrayOfFetus = [`None of you could beat that lol if u change your mind appreciate`,
+                            `Almost as good as Clicker Heroes`,
+                            `Why should you torture yourself with this game when you can do 100 easy ones?`,
+                            `And it's not even guaranteed you're gonna win this game.`,
+                            `Many friends also say this is a hardcore game :D`];
 };
