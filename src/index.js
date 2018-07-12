42e3ef4487731a62a9e91b81de55bf9ca3030526
Anna Gadacz <anna.gadacz@relayr.io>
2018-07-12 14:03:47 +0200

    :art: code readability improvement

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './styles/index.css'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root'))
+registerServiceWorker()
