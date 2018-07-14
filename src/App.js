3a4f0f5e80dc5a9c11fbc1aa68865c66b57b06e7
Arcyvilk <blazemakerrr@gmail.com>
2018-07-14 21:28:12 +0200

    :building_construction: reorganized HTML structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
-import './styles/App.css'
+import './styles/css/App.css'
 import Wrapper from './components/Wrapper'
+import Nav from './components/Nav'
 
 export default class App extends Component {
   render() {
     return (
       <Wrapper type='main'>
-        <Wrapper type='header' />
-        <Wrapper type='nav' />
-        <Wrapper type='content' />
+        <Wrapper type='header'>
+          0.1% - games that masochists love.
+        </Wrapper>
+        <Wrapper type='nav'>
+          <Nav />
+        </Wrapper>
+        <Wrapper type='middle'>
+          <Wrapper type='content' />
+          <Wrapper type='sidebar' />
+        </Wrapper>
         <Wrapper type='footer' />
       </Wrapper>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './styles/index.css'
+import './styles/css/index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
