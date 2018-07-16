a9032f0440242fd1616775514a0edce1685c94bb
Arcyvilk <blazemakerrr@gmail.com>
2018-07-16 14:10:25 +0200

    :art: Rewriting backend and frontend config a bit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,39 +1,12 @@
 import React from 'react'
-
-const items = [
-    { 
-        text: "homepage",
-        icon: "fas fa-home",
-        link: "/"
-    },
-    {
-        text: "members",
-        icon: "fas fa-users",
-        link: "/members"
-    },
-    {
-        text: "game list",
-        icon: "fas fa-gamepad",
-        link: "/games"
-    },
-    {
-        text: "event log",
-        icon: "fas fa-history",
-        link: "events"
-    },
-    {
-        text: "curator",
-        icon: "fab fa-steam",
-        link: "curator"        
-    }
-]
+import tabs from '../config/tabs.json'
 
 export default class Nav extends React.Component{
     render() {
         return (
             <ul className='flex-row'>
                 {
-                    items.map((item, index) => 
+                    tabs.map((item, index) => 
                         <li 
                             key={ `nav-${index}` }
                             className='flex-column'>
