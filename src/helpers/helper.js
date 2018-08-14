cc5b8cbd779f344ae6e756283a5b479e338b6c01
Arcyvilk <blazemakerrr@gmail.com>
2018-08-14 21:17:35 +0200

    :lipstick: Game choice checkboxes are now font awesome icons - helper file

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,3 +1,3 @@
 import rating from '../config/rating'
 
-export const swapRatingToIcon = score => rating.find(r => r.score === score).icon
+export const swapRatingToIcon = score => rating.find(r => r.score === score).link
