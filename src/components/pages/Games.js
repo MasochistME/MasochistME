492f7fadd6da43654b838eaeb0794a61396bdc2a
Arcyvilk <blazemakerrr@gmail.com>
2018-08-25 17:50:29 +0200

    :construction_site: Preparing client to properly update Curated Games

--- src/components/pages/Games.js
+++ src/components/pages/Games.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import axios from 'axios'
+import _ from 'lodash'
 import CheckBoxGameChoice from '../CheckBoxGameChoice'
 import SearchBar from '../SearchBar'
 import rating from '../../config/rating'
-import games from '../../mock/games' //THIS IS FOR DEV PURPOSES - GONNA BE CHANGED TO JSON DOWNLOADED FRMO THE SERVER
+// import games from '../../mock/games.json' //THIS IS FOR DEV PURPOSES - GONNA BE CHANGED TO JSON DOWNLOADED FRMO THE SERVER
 import { swapRatingToIcon } from '../../helpers/helper';
 
 class PageGames extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            games: [ ]
+        }
+        this.updateTimeout = null;
+        this.loadGames = this.loadGames.bind(this)
+    }
+
+    componentDidMount() {
+        this.loadGames()
+    }
+    componentWillUnmount() {
+        clearInterval(this.updateTimeout)
+    }
+
+    loadGames() {
+        axios.get('http://localhost:3001/api/games')
+            .then(response => {
+                if (response.status === 200)
+                    return this.setState({ games: _.orderBy(response.data, ['title', 'score'], ['asc', 'desc']) })
+            })
+            .catch(err => console.log(err.message))
+    }
+
     render() {
         const { props } = this
         return (
@@ -28,7 +55,7 @@ class PageGames extends React.Component{
                 </div>
                 <div className='wrapper-games'>
                     {
-                        games.map(game =>
+                        this.state.games.map(game =>
                             game.title.toLowerCase().indexOf(props.state.searchGame.toLowerCase()) !== -1
                             && props.state.showGamesRated.find(score => parseInt(score,10) === parseInt(game.rating,10))
                             ? <div 
