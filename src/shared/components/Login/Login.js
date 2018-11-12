79e4c5296cd8ed895fc8373f2a7e47995d68fea6
Arcyvilk <blazemakerrr@gmail.com>
2018-11-12 14:28:22 +0100

    :sparkles: User can now access their profile as a tab

--- src/shared/components/Login/Login.js
+++ src/shared/components/Login/Login.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { showLoginModal, logOutUser } from '../../store/modules/Login'
+import { changeTab } from '../../store/modules/Tabs'
 
 class Login extends React.Component{
     showLogin = () => this.props.dispatch(showLoginModal())
+    showProfile = () => this.props.state.logged ? this.props.dispatch(changeTab('profile')) : alert('You are not logged in!')
+    logOut = () => this.props.dispatch(logOutUser())
 
     render() {
         return (
             <div>
                 { !this.props.state.logged
                     ? (
-                        <div className='button flex-row' onClick={ () => this.showLogin() }>
+                        <div className='button flex-row' onClick={ this.showLogin }>
                             <p>Log in</p>
                             <i className="fas fa-sign-in-alt"></i>
                         </div>
                         )
                     : (
                         <div className="flex-row">
-                            <div className='button flex-row' style={{ borderLeft: "none" }} onClick={ () => alert('Profile will be here soon!') }>
+                            <div className='button flex-row' style={{ borderLeft: "none" }} onClick={ this.showProfile }>
                                 <p>{ this.props.state.username }</p>
                             </div>
-                            <div className='button flex-row' onClick={ () => this.props.dispatch(logOutUser()) }>
+                            <div className='button flex-row' onClick={ this.logOut }>
                                 <p>Log out</p>
                                 <i class="fas fa-sign-out-alt"></i>
                             </div>
