b4360f1a16523a55a2ef07e8611dabeed88a8366
Anna Gadacz <anna.gadacz@relayr.io>
2019-04-29 20:26:23 +0200

    :sparkles: help and hmod commands

--- lib/commands/commands/standard.ts
+++ lib/commands/commands/standard.ts
@@ -1,11 +1,29 @@
 import Discord from "discord.js";
 import { chooseRandom } from '../../rng';
-import { removeKeyword,  } from '../../helpers';
+import { removeKeyword, createEmbed } from '../../helpers';
 import { insertData } from '../../db';
 import { cache } from '../../../cache';
 
 // @ts-ignore
-export const meme = (msg:Discord.Message) => msg.channel.send(`_"${chooseRandom(cache.memes).meme}"_`);
+export const help = (msg:Discord.Message) => {
+    let content = '';
+    let embed;
+    cache["commands"]
+        .filter(cmd => !cmd.isModOnly && cmd.description)
+        .map(cmd => content += `- \`\`${cmd.keyword}\`\` - ${cmd.description}\n`);
+    embed = createEmbed('📜 Standard commands', [{ title: 'List:', content }]);
+    msg.channel.send(embed);
+}
+export const hmod = (msg:Discord.Message) => {
+    let content = '';
+    let embed;
+    cache["commands"]
+        .filter(cmd => cmd.isModOnly && cmd.description)
+        .map(cmd => content += `- \`\`${cmd.keyword}\`\` - ${cmd.description}\n`);
+    embed = createEmbed('📜 Moderation commands', [{ title: 'List:', content }]);
+    msg.channel.send(embed);
+}
+export const meme = (msg:Discord.Message) => msg.channel.send(`_"${chooseRandom(cache["memes"]).meme}"_`);
 export const addmeme = (msg:Discord.Message) => insertData('memes', 'meme', removeKeyword(msg), err => 
     err
         ? msg.react('❌')
--- lib/commands/list.ts
+++ lib/commands/list.ts
@@ -10,11 +10,18 @@ import {
     status, 
     impersonate,
 } from './commands/administration';
-import { meme, addmeme } from './commands/standard';
+import { 
+    help,
+    hmod,
+    meme,
+    addmeme,
+} from './commands/standard';
 
 export const Command: { [key:string]: (command:ICommand, msg:Discord.Message) => string | void} = {
+    help: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(help, msg),
+    hmod: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(hmod, msg),
     meme: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(meme, msg),
     addmeme: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(addmeme, msg),
     status: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(status, msg),
-    impersonate: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(impersonate)
+    impersonate: (command:ICommand, msg:Discord.Message) => new CustomCommand(command, msg).execute(impersonate),
 };
