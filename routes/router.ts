1b00f8b49d9ec08dfa754b8e4e0be2e2e6d82455
Arcyvilk <blazemakerrr@gmail.com>
2019-06-23 00:21:27 +0200

    :sparkles: added Patreon related endpoints

--- routes/router.ts
+++ routes/router.ts
@@ -2,9 +2,17 @@ import express from 'express';
 export const router = express.Router();
 
 import { getAllBadges, getBadge, addBadge, updateBadge, deleteBadge } from './badges';
+import { getAllPatrons, getPatronsByTier, getPatron, addPatron, updatePatron, deletePatron } from './patrons';
 
 router.get('/badges', getAllBadges);
 router.post('/badges', addBadge);
 router.get('/badges/:id', getBadge);
 router.put('/badges/:id', updateBadge);
 router.delete('/badges/:id', deleteBadge);
+
+router.get('/patrons', getAllPatrons);
+router.get('/patrons/tier/:tier', getPatronsByTier);
+router.post('/patrons/tier/:tier/vanityid/:vanityid', addPatron);
+router.get('/patrons/patron/:steamid', getPatron);
+router.put('/patrons/patron/:steamid', updatePatron);
+router.delete('/patrons/patron/:steamid', deletePatron);
