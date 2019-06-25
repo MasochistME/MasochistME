de5f4b0ecb78c1376ff1194cea61eee6f9f93929
Arcyvilk <blazemakerrr@gmail.com>
2019-06-25 20:26:07 +0200

    :bug: fixed a bug where CORS blocked patrons request

--- dds.ts
+++ dds.ts
@@ -1,23 +1,17 @@
 import '@babel/polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import config from './config.json';
 import { router } from './routes/router';
 
 const app = express();
 
+
+app.use(cors());
+app.options('*', cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.set({
-      'Access-Control-Request-Method': 'GET',
-      'Access-Control-Allow-Methods': 'OPTIONS, GET',
-      'Access-Control-Allow-Headers': '*',
-      'Access-Control-Allow-Origin': config.CORS
-    });
-    next()
-})
-
 app.use('/rest', router);
 
 app.listen(config.PORT, () => console.log(`Server listening at port ${ config.PORT }!`));
