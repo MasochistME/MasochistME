7292adccabed50d766631710f61f6b63ae5b5f8e
Arcyvilk <blazemakerrr@gmail.com>
2019-08-21 22:50:46 +0200

    :bug: fixing other people overwritting badge creation

--- lib/commands/commands/badges.ts
+++ lib/commands/commands/badges.ts
@@ -172,7 +172,7 @@ const badgeScreenEmbed = (footer?:string) => {
 }
 
 export const badgeCreation = (msg:Discord.Message) => {
-    if (!cache["addbadge"].inProgress || !msg.author.id === cache["addbadge"].author || !msg.channel.id === cache["addbadge"].channel) {
+    if (!cache["addbadge"].inProgress || msg.author.id !== cache["addbadge"].author || msg.channel.id !== cache["addbadge"].channel) {
         return;
     }
     const activeField = cache["addbadge"].activeField;
