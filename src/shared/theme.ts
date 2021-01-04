8b4b631f43547d9758c989bf573c4fe2d93bddec
Arcyvilk <blazemakerrr@gmail.com>
2021-01-04 18:16:22 +0100

    :lipstick: moving sass to styled-components 1

---
+++ src/shared/theme.ts
@@ -0,0 +1,44 @@
+export const colors = {
+  lightYellow: '#b4b59d',
+  lightestGreyEver: '#E9E9F5',
+  lightGrey: '#9e9db5',
+  mediumGrey: '#7b7a8d',
+  darkGrey: '#242630',
+  superDarkGrey: '#141620',
+
+  superLightGrey: '#BEC9E0',
+
+  black: '#000',
+  newDark: '#040404',
+  newMediumGrey: '#545281',
+  darkBlue: '#293251',
+  darkRed: '#1e1012',
+
+  darkBlueTransparent: 'rgba("#293251", 0.7)',
+  darkRedTransparent: 'rgba("#1e1012", 0.7)',
+
+  tier4: '#fdc000',
+  tier3: '#f5f5f5',
+  tier2: '#ff6b1f',
+  tier1: '#32a042',
+
+  tier4Muted: '#a47b00',
+  tier4Transparent: '#403000ad',
+};
+
+export const media = {
+  netbooks: 1100,
+  smallNetbooks: 850,
+  tablets: 650,
+  smallTablets: 500,
+  bigPhones: 400,
+};
+
+export const fonts = {
+  fontCardo: '"Cardo", Verdana, sans-serif',
+  fontCinzel: '"Cinzel", Georgia, serif',
+  fontDosis: '"Dosis", Verdana, sans-serif',
+  fontRaleway: '"Raleway", Verdana, sans-serif',
+  fontPress: '"Press Start 2P", Verdana, sans-serif',
+  fontVerdana: 'Verdana, Geneva, Tahoma, sans-serif',
+};
