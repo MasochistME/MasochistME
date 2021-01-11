5cc03dd271daf0ef29735995b7bd4f3011b50ef0
Arcyvilk <blazemakerrr@gmail.com>
2021-01-11 12:20:14 +0100

    :sparkles: badgelist now shows game name

--- src/commands/commands/badges/badgelist.ts
+++ src/commands/commands/badges/badgelist.ts
@@ -6,14 +6,30 @@ import { log } from "utils/log";
 
 export const badgelist = async (msg: Discord.Message): Promise<void> => {
   let content = "";
-  const url = "http://localhost:3002/api/badges";
+  const urlBadges = "http://89.47.165.141:3002/api/badges";
+  const urlGames = "http://89.47.165.141:3002/api/curator/games";
   try {
-    const badges = await axios.get(url);
-    const filteredBadges = orderBy(badges?.data, ["gameId"], ["asc"]);
-    filteredBadges.map(badge => {
-      const newBadge = `\`\`${badge._id}\`\` - ${badge.name.toUpperCase()} - ${
-        badge.description
-      }\n`;
+    const badges = await axios.get(urlBadges);
+    const games = await axios.get(urlGames);
+    const mappedBadges = badges?.data.map((badge: any) => {
+      const game = games?.data.find((g: any) => {
+        return Number(g.id) === Number(badge.gameId);
+      });
+      return {
+        ...badge,
+        gameName: game?.title ?? badge.game ?? "<unknown>",
+      };
+    });
+
+    const orderedBadges = orderBy(
+      mappedBadges,
+      [badge => badge.gameName],
+      ["asc"],
+    );
+    orderedBadges.map(badge => {
+      const newBadge = `- ${badge.gameName} - \`\`${
+        badge._id
+      }\`\` - ${badge.name.toUpperCase()} - ${badge.description}\n`;
       if (`${content}${newBadge}`.length >= 1024) {
         const embed = createEmbed("🥇 List of badges", [
           { title: "___", content },
