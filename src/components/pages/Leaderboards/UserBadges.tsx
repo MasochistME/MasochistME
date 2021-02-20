905aec6d492d8b4db49620cbfa3f3cae6ce6b219
Arcyvilk <blazemakerrr@gmail.com>
2021-02-20 20:13:34 +0100

    :sparkles: badges on game profiles (#50)

--- src/components/pages/Leaderboards/UserBadges.tsx
+++ src/components/pages/Leaderboards/UserBadges.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Flex } from 'shared/components';
 
-export default function UserBadges(): JSX.Element {
-  // props: {
-  // user: any;
-  // game: any;
-  // }
-  // const { user, game } = props;
+const Badge = styled.img`
+  max-width: 24px;
+  max-height: 24px;
+  border: 3px solid black;
+  border-radius: 3px;
+  margin-left: 4px;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`;
 
-  // const badges = useSelector((state: any) =>
-  //   state.badges.filter((badge: any) =>
-  //     game?.badges?.includes(
-  //       badge['_id'] && user?.badges?.includes([badge['_id']]),
-  //     ),
-  //   ),
-  // );
+export default function UserBadges(props: {
+  user: any;
+  game: any;
+}): JSX.Element {
+  const { user, game } = props;
 
-  return <div></div>;
+  const badges = useSelector((state: any) =>
+    state.badges.filter(
+      (badge: any) =>
+        game?.badges?.includes(badge['_id']) &&
+        user?.badges?.includes(badge['_id']),
+    ),
+  );
+
+  const mappedBadges = badges.map((badge: any) => {
+    const title = `${badge.name} (${badge.points} pts)\n"${badge.description}"`;
+    return (
+      <Badge
+        src={badge.img}
+        alt={badge._id}
+        key={`img-badge-${badge._id}`}
+        title={title}
+      />
+    );
+  });
+
+  return <Flex>{mappedBadges}</Flex>;
 }
