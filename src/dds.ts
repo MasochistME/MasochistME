a445ae25786b24f9868e960d3bd44addef422cfd
Arcyvilk <blazemakerrr@gmail.com>
2021-05-23 21:34:01 +0200

    :sparkles: saving sessions in cookies

--- src/dds.ts
+++ src/dds.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import passport from 'passport';
+import cookieSession from 'cookie-session';
 import { getDataFromDB } from 'helpers/db';
 // import { log } from 'helpers/log';
 import { router } from 'router';
@@ -24,24 +25,39 @@ passport.deserializeUser((obj, done) => {
 passport.use(
   new SteamStrategy(
     {
-      returnURL: 'http://localhost:3002/auth/steam/success',
+      returnURL: 'http://localhost:3002/auth/steam/redirect',
       realm: 'http://localhost:3002/',
       apiKey: config.STEAM_KEY,
     },
     (identifier, profile, done) => {
-      process.nextTick(() => {
-        profile.identifier = identifier;
-        return done(null, profile);
-      });
+      const userData = {
+        name: profile?._json?.personaname ?? 'UNKNOWN ENTITY',
+        id: profile?._json?.steamid ?? -1,
+        identifier,
+      };
+      return done(null, userData);
     },
   ),
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: 'http://localhost:3000',
+  }),
+);
+app.use(
+  cookieSession({
+    name: 'steam-session',
+    keys: [config.STEAM_KEY],
+    maxAge: 30 * 60 * 1000, // session will expire after 30 minutes
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api', router);
 app.use('/auth', routerAuth);
--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,30 +1,44 @@
 import express from 'express';
 import passport from 'passport';
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 
 export const routerAuth = express.Router();
+const CLIENT_HOME_PAGE_URL = 'http://localhost:3000/home';
+const CLIENT_ERROR_PAGE_URL = 'http://localhost:3000/error';
+
+routerAuth.get('/steam', passport.authenticate('steam'));
+
+routerAuth.get('/steam/success', (req: any, res: any) => {
+  // Successful authentication, redirect home.
+  if (req.user) {
+    const userData = {
+      success: true,
+      message: 'User has successfully authenticated!',
+      user: req.user,
+      cookies: req.cookies,
+    };
+    res.json(userData);
+  } else {
+    res.status(401).json({ error: 'User is not authenticated.' });
+  }
+});
+
+routerAuth.get('/steam/error', (req: any, res: any) => {
+  res.status(401).json({
+    success: false,
+    message: 'User failed to authenticate!',
+  });
+});
 
 routerAuth.get(
-  '/steam',
-  passport.authenticate('steam', { failureRedirect: '/steam/error' }),
+  '/steam/redirect',
+  passport.authenticate('steam', {
+    successRedirect: CLIENT_HOME_PAGE_URL,
+    failureRedirect: CLIENT_ERROR_PAGE_URL,
+  }),
 );
 
-routerAuth.get(
-  '/steam/success',
-  passport.authenticate('steam', { failureRedirect: '/steam/error' }),
-  (req: any, res: any) => {
-    // Successful authentication, redirect home.
-    if (req.user) {
-      const userId = req.user.id;
-      const token = uuid();
-      res.redirect(`http://localhost:3000?userId=${userId}&token=${token}`);
-    } else {
-      res.redirect('http://localhost:3000/error');
-    }
-  },
-);
-
-routerAuth.get(
-  '/steam/logout',
-  passport.authenticate('steam', { failureRedirect: '/steam/error' }),
-);
+routerAuth.get('/steam/logout', (req: any, res: any) => {
+  req.logout();
+  res.redirect(CLIENT_HOME_PAGE_URL);
+});
