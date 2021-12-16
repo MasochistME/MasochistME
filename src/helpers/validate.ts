4ace103ee6b72ade8e640542f668437e1a2cba5e
Arcyvilk <blazemakerrr@gmail.com>
2021-12-16 01:04:49 +0100

    refactor: 💡 add hashing to validation

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -1,3 +1,4 @@
+import { hash } from 'helpers/hash';
 import { log } from 'helpers/log';
 
 export const tokenValidation = (req: any, res: any, next) => {
@@ -9,7 +10,9 @@ export const tokenValidation = (req: any, res: any, next) => {
     log.CRITICAL('-- Fake token: NONE');
     return res.sendStatus(401);
   }
-  if (authorizationHeader !== process.env.TEMP_ACCESS_TOKEN) {
+  if (
+    hash('sha256', authorizationHeader) !== process.env.HASH_TEMP_ACCESS_TOKEN
+  ) {
     log.CRITICAL('An unauthorized attempt to access protected endpoint!');
     log.CRITICAL(`-- Endpoint: ${req.url}`);
     log.CRITICAL(`-- Fake token: ${authorizationHeader}`);
