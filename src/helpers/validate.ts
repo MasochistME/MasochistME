49f06182abbf09e449d670a7f309a2a098b79e1e
Arcyvilk <blazemakerrr@gmail.com>
2021-12-18 16:36:11 +0100

    refactor: 💡 sanitize logs

--- src/helpers/validate.ts
+++ src/helpers/validate.ts
@@ -3,21 +3,19 @@ import { log } from 'helpers/log';
 
 export const tokenValidation = (req: any, res: any, next) => {
   const authorizationHeader = req.headers.authorization;
+  const url = String(req.url ?? '<UNKNOWN>');
+  const fakeToken = String(authorizationHeader ?? 'NONE');
 
-  if (!authorizationHeader) {
-    log.CRITICAL('An unauthorized attempt to access protected endpoint!');
-    log.CRITICAL(`-- Endpoint: ${req.url}`);
-    log.CRITICAL('-- Fake token: NONE');
-    return res.sendStatus(401);
-  }
   if (
+    !authorizationHeader ||
     hash('sha256', authorizationHeader) !== process.env.HASH_TEMP_ACCESS_TOKEN
   ) {
     log.CRITICAL('An unauthorized attempt to access protected endpoint!');
-    log.CRITICAL(`-- Endpoint: ${req.url}`);
-    log.CRITICAL(`-- Fake token: ${authorizationHeader}`);
+    log.CRITICAL(`-- Endpoint: ${url}`);
+    log.CRITICAL(`-- Fake token: ${fakeToken}`);
     return res.sendStatus(401);
   }
+  log.INFO(`Authorized access to endpoint: ${url}`);
 
   next();
 };
