117618c0637b97649b99210165de66877206e487
Arcyvilk <blazemakerrr@gmail.com>
2022-10-05 23:20:34 +0200

    refactor: 💡 temporarily disable passport

--- src/dds.ts
+++ src/dds.ts
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 import express from 'express';
 import cors from 'cors';
-import passport from 'passport';
+// import passport from 'passport';
 import cookieParser from 'cookie-parser';
-import session from 'express-session';
+// import session from 'express-session';
 
 import { tokenValidation } from 'helpers/validate';
 import { getDataFromDB } from 'helpers/db';
@@ -16,45 +16,45 @@ import { initiateMainUpdate } from 'router/update';
 
 import config from '../config.json';
 
-const SteamStrategy = require('passport-steam').Strategy;
+// const SteamStrategy = require('passport-steam').Strategy;
 const app = express();
 
-const rootPath =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost'
-    : 'http://89.47.165.141';
+// const rootPath =
+//   process.env.NODE_ENV === 'development'
+//     ? 'http://localhost'
+//     : 'http://89.47.165.141';
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
 
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
+// passport.deserializeUser((obj, done) => {
+//   done(null, obj);
+// });
 
-passport.use(
-  new SteamStrategy(
-    {
-      returnURL: `${rootPath}:3002/auth/steam/redirect`,
-      realm: `${rootPath}:3002/`,
-      apiKey: process.env.STEAM_KEY,
-    },
-    async (identifier, profile, done) => {
-      process.nextTick(() => {
-        const member = getDataFromDB('members', {
-          id: profile?._json?.steamid ?? -1,
-        });
-        const userData = {
-          name: profile?._json?.personaname ?? 'UNKNOWN ENTITY',
-          id: Number(profile?._json?.steamid ?? -1),
-          identifier,
-          permissions: member?.groups ?? [],
-        };
-        return done(null, userData);
-      });
-    },
-  ),
-);
+// passport.use(
+//   new SteamStrategy(
+//     {
+//       returnURL: `${rootPath}:3002/auth/steam/redirect`,
+//       realm: `${rootPath}:3002/`,
+//       apiKey: process.env.STEAM_KEY,
+//     },
+//     async (identifier, profile, done) => {
+//       process.nextTick(() => {
+//         const member = getDataFromDB('members', {
+//           id: profile?._json?.steamid ?? -1,
+//         });
+//         const userData = {
+//           name: profile?._json?.personaname ?? 'UNKNOWN ENTITY',
+//           id: Number(profile?._json?.steamid ?? -1),
+//           identifier,
+//           permissions: member?.groups ?? [],
+//         };
+//         return done(null, userData);
+//       });
+//     },
+//   ),
+// );
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: false }));
@@ -70,14 +70,14 @@ app.use(
 );
 app.use(cookieParser());
 app.set('trust proxy', 1);
-app.use(
-  session({
-    secret: process.env.AUTH,
-    cookie: { name: 'steam-session', maxAge: 360000, secure: false },
-  }),
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//   session({
+//     secret: process.env.AUTH,
+//     cookie: { name: 'steam-session', maxAge: 360000, secure: false },
+//   }),
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // this is for prod env where req.user does not exist
 app.use((req: any, res: any, next) => {
