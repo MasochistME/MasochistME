c8ebddf6e6a7d38e7b7fb7abafd285bcb495c615
Arcyvilk <blazemakerrr@gmail.com>
2022-10-05 23:48:23 +0200

    refactor: 💡 connect discord user to steam with api use

--- src/commands/buttons/registrationReview.ts
+++ src/commands/buttons/registrationReview.ts
@@ -1,7 +1,9 @@
+import axios from "axios";
 import { getErrorEmbed } from "arcybot";
 import { ButtonInteraction, APIEmbed } from "discord.js";
 
 import { isMod } from "utils";
+import { API_URL } from "consts";
 
 export const REGISTRATION_REVIEW = "REGISTRATION_REVIEW";
 
@@ -27,8 +29,10 @@ export const registrationReview = async (
   const embedFields = interaction.message.embeds[0].data.fields;
   const steamId = embedFields?.find(field => field.name === "Steam ID")?.value;
   const user = embedFields?.find(field => field.name === "User")?.value;
+  const regexUserId = new RegExp(/(?<=<@)([^>]*)/);
+  const userDiscordId = user?.match(regexUserId)?.[0];
 
-  if (!steamId || !user) {
+  if (!steamId || !user || !userDiscordId) {
     interaction.update({
       ...getErrorEmbed("Error", "Something went wrong."),
       components: [],
@@ -36,6 +40,56 @@ export const registrationReview = async (
     return;
   }
 
+  const embed = getApplicationReviewedEmbed(interaction, user, steamId);
+
+  if (interaction.customId === `${REGISTRATION_REVIEW}_APPROVE`) {
+    try {
+      await connectDiscordUserToSteam(steamId, userDiscordId);
+    } catch (err: any) {
+      interaction.update({
+        ...getErrorEmbed("Error", err),
+        components: [],
+      });
+    }
+    interaction.update({
+      embeds: [{ ...embed, title: "✅ User application - APPROVED" }],
+      components: [],
+    });
+  }
+  if (interaction.customId === `${REGISTRATION_REVIEW}_REJECT`) {
+    interaction.update({
+      embeds: [{ ...embed, title: "❌ User application - REJECTED" }],
+      components: [],
+    });
+  }
+};
+
+/**
+ * Sends the request to API to connect Discord user with Masochist.ME one.
+ * @param steamId string
+ * @param discordId string
+ */
+const connectDiscordUserToSteam = async (
+  steamId: string,
+  discordId: string,
+) => {
+  const url = `${API_URL}/users/user/${steamId}/discord/${discordId}`;
+  const badgeRes = await axios.put(url);
+  if (badgeRes.status !== 200) throw badgeRes.data;
+};
+
+/**
+ * Creates and returns embed with info about the result of registration review.
+ * @param interaction ButtonInteraction
+ * @param user string - user's Discord mention string
+ * @param steamId string - user's Steam ID
+ * @returns APIEmbed
+ */
+const getApplicationReviewedEmbed = (
+  interaction: ButtonInteraction,
+  user: string,
+  steamId: string,
+): APIEmbed => {
   const getVerdict = (): "APPROVED" | "REJECTED" | "UNKNOWN" => {
     if (interaction.customId === `${REGISTRATION_REVIEW}_APPROVE`)
       return "APPROVED";
@@ -44,7 +98,7 @@ export const registrationReview = async (
     return "UNKNOWN";
   };
 
-  const embed: APIEmbed = {
+  return {
     fields: [
       {
         name: "User",
@@ -70,17 +124,4 @@ export const registrationReview = async (
       },
     ],
   };
-
-  if (interaction.customId === `${REGISTRATION_REVIEW}_APPROVE`) {
-    interaction.update({
-      embeds: [{ ...embed, title: "✅ User application - APPROVED" }],
-      components: [],
-    });
-  }
-  if (interaction.customId === `${REGISTRATION_REVIEW}_REJECT`) {
-    interaction.update({
-      embeds: [{ ...embed, title: "❌ User application - REJECTED" }],
-      components: [],
-    });
-  }
 };
