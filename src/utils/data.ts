e3efd5dfe37ecd93980a55e067756226c4851b01
Arcyvilk <blazemakerrr@gmail.com>
2022-10-05 21:42:43 +0200

    refactor: 💡 additional util options for cache data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,5 +1,21 @@
+import { DiscordInteraction } from "arcybot";
+
 import { cache } from "fetus";
 import { UNKNOWN_NAME } from "consts";
+
+export const getOption = (key: string) =>
+  cache.options.find(option => option.option === key)?.value;
+
+export const getChannelById = (
+  interaction: DiscordInteraction,
+  channelId?: string,
+) => {
+  const channel = interaction.guild?.channels.cache.find(
+    ch => ch.id === channelId,
+  );
+  if (channel?.isTextBased()) return channel;
+  return null;
+};
 
 export const getBadgeNameById = (id?: string | null): string => {
   if (!id) return UNKNOWN_NAME;
