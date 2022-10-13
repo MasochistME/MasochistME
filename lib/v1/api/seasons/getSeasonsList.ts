26ca730a48f96c6301a32141556011f4c01f01fb
Arcyvilk <blazemakerrr@gmail.com>
2022-10-13 22:29:41 +0200

    fix: 🐛 add missing getseasonslist method

---
+++ lib/v1/api/seasons/getSeasonsList.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse } from 'axios';
+
+import { Season, ResponseError } from 'v1/types';
+
+/**
+ * Returns a list of all seasons.
+ * @category Seasons
+ * @param params.finished - Get only seasons which are finished.
+ * @param params.inactive - Get only seasons which are yet to start.
+ */
+export const getSeasonsList = async (
+	params: { finished?: boolean; inactive?: boolean },
+	/** @ignore */
+	BASE_URL: string,
+): Promise<Season[]> => {
+	const { finished, inactive } = params;
+	const url = `${BASE_URL}/seasons/list`;
+
+	const seasonResponse = await axios.post<
+		Season[] | ResponseError,
+		AxiosResponse<Season[] | ResponseError>
+	>(url, { finished, inactive }, { validateStatus: () => true });
+
+	const { status, data } = seasonResponse;
+
+	if (status !== 200) throw new Error((data as ResponseError).error);
+	return data as Season[];
+};
