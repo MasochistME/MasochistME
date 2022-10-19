1565a2ad3c8a2c8c406efdd4207f4c1551036329
Arcyvilk <blazemakerrr@gmail.com>
2022-10-19 15:31:15 +0200

    refactor: 💡 make Flex component MUCH better

--- src/components/layout/Flex.tsx
+++ src/components/layout/Flex.tsx
@@ -5,26 +5,18 @@ type Props = {
 	column?: boolean;
 	justify?: boolean;
 	align?: boolean;
-	style?: React.CSSProperties;
-};
+} & React.CSSProperties;
 
 export const Flex = styled.div.attrs((props: Props) => {
-	const { row, column, justify, align, style } = props;
-	const newStyle = { ...style };
-	if (row) {
-		newStyle.flexDirection = 'row';
-	}
-	if (column) {
-		newStyle.flexDirection = 'column';
-	}
-	if (justify) {
-		newStyle.justifyContent = 'center';
-	}
-	if (align) {
-		newStyle.alignItems = 'center';
-	}
+	const { row, column, justify, align, ...style } = props;
+	const newStyle: React.CSSProperties = {};
+	if (row) newStyle.flexDirection = 'row';
+	if (column) newStyle.flexDirection = 'column';
+	if (justify) newStyle.justifyContent = 'center';
+	if (align) newStyle.alignItems = 'center';
 	return {
 		style: newStyle,
+		...style,
 	};
 })<Props>`
 	display: flex;
