afc4aee252ab0bbbca3d89bf3470de02d189fc2b
Arcyvilk <blazemakerrr@gmail.com>
2022-10-24 00:10:42 +0200

    fix: 🐛 fix express bug with empty callback

--- lib/router/v1/update/updateCurator/getUpdateCuratorStatus.ts
+++ lib/router/v1/update/updateCurator/getUpdateCuratorStatus.ts
@@ -1,3 +1,8 @@
-export const getUpdateCuratorStatus = () => {
-  return 'TODO';
+import { Request, Response } from 'express';
+
+export const getUpdateCuratorStatus = async (
+  _req: Request,
+  res: Response,
+): Promise<void> => {
+  res.status(404).send('TODO');
 };
