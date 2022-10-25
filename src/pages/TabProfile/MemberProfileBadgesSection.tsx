b33bd97d109f44101476f2f32e1f5f3f1ccb3b14
Arcyvilk <blazemakerrr@gmail.com>
2022-10-25 15:44:15 +0200

    fix: 🐛 fix ghost badge number

--- src/pages/TabProfile/MemberProfileBadgesSection.tsx
+++ src/pages/TabProfile/MemberProfileBadgesSection.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
-import { MemberBadge, Badge, Game } from '@masochistme/sdk/dist/v1/types';
 
 import { Flex } from 'components';
 import { BadgeThumbnail, Section } from 'containers';
-import { useActiveTab } from 'shared/hooks';
+import { useActiveTab, useMemberBadgesFilter } from 'shared/hooks';
 import { TabDict } from 'shared/config/tabs';
-import { useAllGames, useBadges, useMemberBadges } from 'sdk';
 import { Size } from 'utils';
 
 type Props = {
@@ -20,37 +18,20 @@ export const MemberProfileBadgesSection = (props: Props): JSX.Element => {
 
 	useActiveTab(TabDict.PROFILE);
 
-	const { gamesData: games } = useAllGames();
-	const { memberBadgesData = [] } = useMemberBadges(memberId);
-	const { badgesData } = useBadges({
-		sort: { points: 'desc' },
-	});
+	const memberBadgesData = useMemberBadgesFilter(memberId);
 
 	const onBadgeClick = (gameId: number | null) => {
 		if (gameId) history.push(`/game/${gameId}`);
 	};
 
-	const memberBadges = badgesData
-		.filter(badge => {
-			return memberBadgesData.find(
-				(mb: MemberBadge) => mb.badgeId === String(badge._id),
-			);
-		})
-		.map(badge => {
-			if (!badge) return;
-			const game = games.find((g: Game) => g.id === badge.gameId);
-			if (!game || game.isCurated || game.isProtected) {
-				return (
-					<BadgeThumbnail
-						badge={badge as Badge}
-						size={Size.BIG}
-						key={`member-badge-${String(badge._id)}`}
-						onClick={() => onBadgeClick(badge.gameId)}
-					/>
-				);
-			}
-		})
-		.filter(Boolean);
+	const memberBadges = memberBadgesData.map(badge => (
+		<BadgeThumbnail
+			badge={badge}
+			size={Size.BIG}
+			key={`member-badge-${String(badge._id)}`}
+			onClick={() => onBadgeClick(badge.gameId)}
+		/>
+	));
 
 	return (
 		<Section
---
+++ src/shared/hooks/useMemberBadgesFilter.ts
@@ -0,0 +1,23 @@
+import { MemberBadge, Game } from '@masochistme/sdk/dist/v1/types';
+
+import { useAllGames, useBadges, useMemberBadges } from 'sdk';
+
+export const useMemberBadgesFilter = (memberId: string) => {
+	const { gamesData: games } = useAllGames();
+	const { memberBadgesData = [] } = useMemberBadges(memberId);
+	const { badgesData } = useBadges({
+		sort: { points: 'desc' },
+	});
+
+	const memberBadges = badgesData.filter(badge => {
+		const game = games.find((g: Game) => g.id === badge.gameId);
+		const isMemberHasBadge = memberBadgesData.find(
+			(mb: MemberBadge) => mb.badgeId === String(badge._id),
+		);
+		if (!isMemberHasBadge) return false;
+		if (!game || game.isCurated || game.isProtected) return true;
+		return false;
+	});
+
+	return memberBadges;
+};
