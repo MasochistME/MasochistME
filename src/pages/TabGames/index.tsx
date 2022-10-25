a1deea5f441a9cb4b86148d086d4e0b464619bf8
Arcyvilk <blazemakerrr@gmail.com>
2022-10-25 16:54:21 +0200

    refactor: 💡 remove info section from games page

--- src/pages/TabGames/index.tsx
+++ src/pages/TabGames/index.tsx
@@ -19,42 +19,44 @@ const TabGames = (): JSX.Element => {
 	return (
 		<SubPage>
 			<StyledGames column>
-				<TabGamesInfo isMobileOnly />
 				<GameFilterBar />
 				{gameListView === GameView.TILE && <GameTileView />}
 				{gameListView === GameView.TABLE && <GameTableView />}
 			</StyledGames>
-			<TabGamesInfo isDesktopOnly width="100%" maxWidth="450px" />
 		</SubPage>
 	);
 };
 
-const TabGamesInfo = (props: Partial<SectionProps>): JSX.Element => {
-	return (
-		<Section
-			{...props}
-			title="MasochistME curations"
-			content={
-				<Flex column gap={8}>
-					<div>
-						Here&lsquo;s the list of games that MasochistME curates, as well as
-						the percentage completion comparision between our users.
-					</div>
-					<div>
-						In the MasochistME community, we grade the ranks of our users by how
-						many curated games they&lsquo;ve completed, as well as the
-						difficulty of those games. Each game specifies their own difficulty
-						in the description.
-					</div>
-					<div>
-						The list also includes which three users completed the game first
-						(with a gold, silver and bronze medals, respectively).
-					</div>
-				</Flex>
-			}
-		/>
-	);
-};
+// /**
+// * This is temporarily disabled since there is no use for it.
+// * Might be reenabled in the future where game events are introduced.
+// */
+// const TabGamesInfo = (props: Partial<SectionProps>): JSX.Element => {
+// 	return (
+// 		<Section
+// 			{...props}
+// 			title="MasochistME curations"
+// 			content={
+// 				<Flex column gap={8}>
+// 					<div>
+// 						Here&lsquo;s the list of games that MasochistME curates, as well as
+// 						the percentage completion comparision between our users.
+// 					</div>
+// 					<div>
+// 						In the MasochistME community, we grade the ranks of our users by how
+// 						many curated games they&lsquo;ve completed, as well as the
+// 						difficulty of those games. Each game specifies their own difficulty
+// 						in the description.
+// 					</div>
+// 					<div>
+// 						The list also includes which three users completed the game first
+// 						(with a gold, silver and bronze medals, respectively).
+// 					</div>
+// 				</Flex>
+// 			}
+// 		/>
+// 	);
+// };
 
 export default TabGames;
 
