aa8be62ec33b8435eb3954ee54b4263f00c73b08
Arcyvilk <blazemakerrr@gmail.com>
2022-11-08 22:34:31 +0100

    fix: 🐛 leaderboards filter not resetting

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -5,4 +5,3 @@ export * from './useLocalStorage';
 export * from './useMemberBadgesFilter';
 export * from './useMemberLeaderboardsPoints';
 export * from './useMemberLeaderboardsSummary';
-export * from './useRankingList';
