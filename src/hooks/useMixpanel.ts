a8a5da6d5542333bd55258f6450118140a30b8be
Arcyvilk <blazemakerrr@gmail.com>
2023-02-05 18:24:12 +0100

    refactor: 💡 more mixpanel fixes

--- src/hooks/useMixpanel.ts
+++ src/hooks/useMixpanel.ts
@@ -1,19 +1,21 @@
 import mixpanel from 'mixpanel-browser';
 import config from 'config.json';
 
+let USE_MIXPANEL = false;
 // @ts-ignore
 if (config.MIXPANEL_TOKEN) {
 	// @ts-ignore
 	mixpanel.init(config.MIXPANEL_TOKEN, { debug: false });
+	USE_MIXPANEL = true;
 }
 
 export const useMixpanel = () => {
 	const track = (event: string, params = {}) => {
-		mixpanel.track(event, params);
+		if (USE_MIXPANEL) mixpanel.track(event, params);
 	};
 
 	const trackLink = (domId: string, event: string, params = {}) => {
-		mixpanel.track_links(domId, event, params);
+		if (USE_MIXPANEL) mixpanel.track_links(domId, event, params);
 	};
 
 	return { track, trackLink };
