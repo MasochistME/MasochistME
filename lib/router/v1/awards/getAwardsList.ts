5128d8eba98e142d1ac2a760766b34404bb802a9
Arcyvilk <blazemakerrr@gmail.com>
2023-04-05 19:34:04 +0200

    refactor: 💡 sort awards list to their respective categories

--- lib/router/v1/awards/getAwardsList.ts
+++ lib/router/v1/awards/getAwardsList.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { Award } from '@masochistme/sdk/dist/v1/types';
+import { Award, AwardCategory } from '@masochistme/sdk/dist/v1/types';
 import { AwardsListParams } from '@masochistme/sdk/dist/v1/api/awards';
 
 import { log } from 'helpers/log';
@@ -14,21 +14,42 @@ export const getAwardsList = async (
 ): Promise<void> => {
   try {
     const { filter = {}, sort = {}, limit = 1000 } = req.body;
-
     const { db } = mongoInstance.getDb();
-    const collection = db.collection<Award>('awards');
-    const awards: Award[] = [];
 
-    const cursor = collection
+    /**
+     * Get all the award categories
+     */
+    const collectionAwardCategories =
+      db.collection<AwardCategory>('awardCategories');
+    const awardCategories: AwardCategory[] = [];
+    const cursorCategories = collectionAwardCategories.find();
+
+    await cursorCategories.forEach((category: AwardCategory) => {
+      awardCategories.push(category);
+    });
+
+    /**
+     * Get all the awards and assign them to categories
+     */
+    const collectionAwards = db.collection<Award>('awards');
+    const awards: Record<string, Award[]> = {};
+    const cursorAwards = collectionAwards
       .find(filter)
       // .sort(sort) // This is unused atm
       .limit(limit);
 
-    await cursor.forEach((el: Award) => {
-      awards.push(el);
+    await cursorAwards.forEach((award: Award) => {
+      const awardCategory = award.category;
+      awards[awardCategory] = [...(awards[awardCategory] ?? []), award];
     });
 
-    res.status(200).send(awards);
+    const fixedCategories: (AwardCategory & { awards: Award[] })[] =
+      awardCategories.map(category => ({
+        ...category,
+        awards: awards[category.humanReadableId],
+      }));
+
+    res.status(200).send(fixedCategories);
   } catch (err: any) {
     log.WARN(err);
     res.status(500).send({ error: err.message ?? 'Internal server error' });
