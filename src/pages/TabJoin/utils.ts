dd14cfda73d9ed97e06cfeaca90e088f8c46bef2
Arcyvilk <blazemakerrr@gmail.com>
2023-04-16 22:31:53 +0200

    fix: 🐛 regex for candidate url now can include slash at the end

--- src/pages/TabJoin/utils.ts
+++ src/pages/TabJoin/utils.ts
@@ -2,10 +2,10 @@ export const validateSteamUrl = (steamUrl?: string) => {
 	const error =
 		'Steam profile URL must match the format https://steamcommunity.com/id/* OR https://steamcommunity.com/profiles/*';
 	const steamUrlWithIdValidator = new RegExp(
-		/^https:\/\/steamcommunity\.com\/profiles\/[0-9]*$/i,
+		/^https:\/\/steamcommunity\.com\/profiles\/[0-9]*[\/]?$/i,
 	);
 	const steamUrlWithNameValidator = new RegExp(
-		/^https:\/\/steamcommunity\.com\/id\/[a-zA-Z0-9_]*$/i,
+		/^https:\/\/steamcommunity\.com\/id\/[a-zA-Z0-9_]*[\/]?$/i,
 	);
 	const hasError =
 		!steamUrl ||
