e187e1446b60cdae99d45a86c828d21398cb67dd
Arcyvilk <blazemakerrr@gmail.com>
2025-06-28 22:45:19 +0200

    refactor: 💡 properly initialize Sentry

--- apps/masochist-web/src/initialize/initializeSentry.ts
+++ apps/masochist-web/src/initialize/initializeSentry.ts
@@ -1,19 +1,21 @@
 import * as Sentry from '@sentry/react';
 
-Sentry.init({
-  dsn: 'https://15f6b6fa71ecf30624cd995585b7d68a@o4509576268218368.ingest.de.sentry.io/4509578229579856',
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+export const initializeSentry = () => {
+  Sentry.init({
+    dsn: 'https://15f6b6fa71ecf30624cd995585b7d68a@o4509576268218368.ingest.de.sentry.io/4509578229579856',
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+};
